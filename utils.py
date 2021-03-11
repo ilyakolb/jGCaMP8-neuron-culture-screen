50c5264d23812777346d2e8dcb6d3644c284cc41
Kolb <kolbi@hhmi.org>
2021-03-11 08:34:41 -0800

    updated xlsx (20210311)
     - xlsx fixed jG7f rise time issues

--- utils.py
+++ utils.py
@@ -27,7 +27,8 @@ def condition_df(data_in):
 	data_in.columns = data_in.columns.str.replace('TimeToPeak', 'Time to peak')
 
 	# replace 1FP with DF/F (1FP)
-	data_in = data_in.rename(columns = {'1 AP': 'DF/F (1 AP)', '3 AP': 'DF/F (3 AP)', '10 AP': 'DF/F (10 AP)', '160 AP': 'DF/F (160 AP)'})
+	data_in = data_in.rename(columns = {'1 AP': 'DF/F (1 AP)', '3 AP': 'DF/F (3 AP)', '10 AP': 'DF/F (10 AP)', '160 AP': 'DF/F (160 AP)',
+										'1 AP(p)': 'DF/F (1 AP)(p)', '3 AP(p)': 'DF/F (3 AP)(p)', '10 AP(p)': 'DF/F (10 AP)(p)', '160 AP(p)': 'DF/F (160 AP)(p)'})
 
 	# remove NaNs
 	data_in.dropna(axis = 0, how = 'any')
